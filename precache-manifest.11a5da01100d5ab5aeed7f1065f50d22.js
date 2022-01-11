4c2a204fb3daefd49850696461d1a345974b4a0a
hushuo <1253740807@qq.com>
2022-01-11 20:22:39 +0800

    deploy

---
+++ precache-manifest.11a5da01100d5ab5aeed7f1065f50d22.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0c633f733da00629c8",
+    "url": "/lemon-morney-website/css/app.7edde18e.css"
+  },
+  {
+    "revision": "78149a577d8487852832",
+    "url": "/lemon-morney-website/css/chunk-vendors.5385c169.css"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/lemon-morney-website/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/lemon-morney-website/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/lemon-morney-website/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/lemon-morney-website/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "968b6b069a04b5322287cb5b7e1c1e53",
+    "url": "/lemon-morney-website/img/lemon.968b6b06.png"
+  },
+  {
+    "revision": "9714722e1fd982fc8f6f9f8c33b78dd3",
+    "url": "/lemon-morney-website/index.html"
+  },
+  {
+    "revision": "fe0c633f733da00629c8",
+    "url": "/lemon-morney-website/js/app.fef51433.js"
+  },
+  {
+    "revision": "78149a577d8487852832",
+    "url": "/lemon-morney-website/js/chunk-vendors.8eec6128.js"
+  },
+  {
+    "revision": "097bf89ffd8c05673159783a074b959d",
+    "url": "/lemon-morney-website/lemonWebsite.png"
+  },
+  {
+    "revision": "166d907e79808a83b2d4c07d754e8852",
+    "url": "/lemon-morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lemon-morney-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lemon-morney-website/precache-manifest.11a5da01100d5ab5aeed7f1065f50d22.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "lemon-moeney-website"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
